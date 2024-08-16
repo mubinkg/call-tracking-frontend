5b9114859bd8e2590fc09b6852a5af4938b9586b
mubin <mubin.ice.ru@gmail.com>
2024-08-17 00:10:46 +0600

    add auth flow

--- src/app/(authentication)/reset-password/page.tsx
+++ src/app/(authentication)/reset-password/page.tsx
@@ -17,9 +17,9 @@ export default function Page() {
       <div className="flex items-center justify-center mt-[25vh]">
         <Card className="mx-auto max-w-sm">
           <CardHeader>
-            <CardTitle className="text-2xl text-center">Login</CardTitle>
+            <CardTitle className="text-2xl text-center">Reset Password</CardTitle>
             <CardDescription>
-              Enter your email below to login to your account
+              Enter your email below to recover your account
             </CardDescription>
           </CardHeader>
           <CardContent>
@@ -33,23 +33,13 @@ export default function Page() {
                   required
                 />
               </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <Link href="/reset-password" className="ml-auto inline-block text-sm underline">
-                    Forgot your password?
-                  </Link>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
               <Button type="submit" className="w-full">
-                Login
+                Reset Password
               </Button>
             </div>
             <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link href="/sign-up" className="underline">
-                Sign up
+              <Link href="/" className="underline">
+                Return to Login
               </Link>
             </div>
           </CardContent>
