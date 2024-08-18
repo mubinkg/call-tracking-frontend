155c6d51aa9c4c76eb836cec6c3a6b34d8ccd31a
mubin <mubin.ice.ru@gmail.com>
2024-08-19 02:41:11 +0600

    add campaign page

--- src/app/(tracker)/campaign/page.tsx
+++ src/app/(tracker)/campaign/page.tsx
@@ -1,7 +1,10 @@
+import CampaignTable from "@/app-component/campaign/CampaignTable";
+
 export default function page(){
     return (
         <div className="m-[2rem]">
             <h1 className="text-lg font-semibold md:text-2xl">Campaing</h1>
+            <CampaignTable/>
         </div>
     )
 }
