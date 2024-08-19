4a96a9ff5a934b35b91d846a2f738196314a1387
mubin <mubin.ice.ru@gmail.com>
2024-08-19 21:09:23 +0600

    add worker page

--- src/app/(tracker)/workspace/page.tsx
+++ src/app/(tracker)/workspace/page.tsx
@@ -1,8 +1,17 @@
+import { Card, CardTitle } from "@/components/ui/card";
+
 export default function page(){
     return (
         <div>
-            <div className="m-[2rem]">
-                <h1 className="text-lg font-semibold md:text-2xl">Workspace</h1>
+            <div className="m-[2rem] flex items-center justify-center">
+                <Card className="lg:w-6/12 md:w-12/12">
+                    <CardTitle>
+                        <div className="m-4">
+                            SETUP YOUR WORKSPACE
+                        </div>
+                        <hr />
+                    </CardTitle>
+                </Card>
             </div>
         </div>
     )
