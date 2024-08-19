4658280772345187a98eeb3500d252ac47c0ba74
mubin <mubin.ice.ru@gmail.com>
2024-08-19 21:33:57 +0600

    add workspace

--- src/app/(tracker)/workspace/page.tsx
+++ src/app/(tracker)/workspace/page.tsx
@@ -1,6 +1,9 @@
-import { Card, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
 
-export default function page(){
+export default function page() {
     return (
         <div>
             <div className="m-[2rem] flex items-center justify-center">
@@ -11,6 +14,46 @@ export default function page(){
                         </div>
                         <hr />
                     </CardTitle>
+                    <CardContent>
+                        <div className="my-3 flex justify-between items-center">
+                            <div>
+                                <h4 className="text-lg mb-1">Name</h4>
+                                <p className="text-sm text-slate-300">Everyone in your workspace can see this</p>
+                            </div>
+                            <Input className="w-4/12" value="Hello World" />
+                        </div>
+                        <hr />
+                        <div className="my-3">
+                            <div className="flex justify-end">
+                                <div className="flex items-center gap-4">
+                                    <SearchIcon />
+                                    <Button className="px-[30px]">Invite</Button>
+                                </div>
+                            </div>
+                            <div>
+                                <p className="text-sm">Members</p>
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+            <div className="m-[2rem] flex items-center justify-center">
+                <Card className="lg:w-6/12 md:w-12/12">
+                    <CardContent>
+                        <div className="my-3 flex justify-between items-center">
+                            <div>
+                                <h4 className="text-xxl mb-1">DESTROY WORKSPACE</h4>
+                                <p className="text-sm text-slate-300">Everyone in your workspace can see this</p>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="flex items-center mt-3">
+                            <h2 className="text-xs">
+                            By deleting account you will loose all your data and access to any workspaces that you have created
+                            </h2>
+                            <Button>Destroy this workspace</Button>
+                        </div>
+                    </CardContent>
                 </Card>
             </div>
         </div>
---
+++ src/icons/Search.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <svg
+            width="24px"
+            height="24px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className='dark:color-[#fff]'
+        >
+            <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+            <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                    d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
+                    stroke="#000000"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />{" "}
+            </g>
+        </svg>
+
+    )
+}
+
+export default Search
