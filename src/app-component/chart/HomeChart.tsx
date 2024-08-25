80e01cd298da55cc61d4a0e6c67e13ff58cd6df6
mubin.khan <mubin.khan@upaybd.com>
2024-08-25 13:31:11 +0600

    add chart on report

---
+++ src/app-component/chart/HomeChart.tsx
@@ -0,0 +1,97 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80, tab:49 },
+  { month: "February", desktop: 305, mobile: 200, tab: 300 },
+  { month: "March", desktop: 237, mobile: 120 , tab: 210},
+  { month: "April", desktop: 73, mobile: 190 , tab: 134},
+  { month: "May", desktop: 209, mobile: 130 , tab: 211},
+  { month: "June", desktop: 214, mobile: 140 ,tab:202},
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  tab: {
+    label: "Tab",
+    color: "hsl(var(--chart-2))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-3))",
+  },
+} satisfies ChartConfig
+
+export function HomeChart() {
+  return (
+    <Card>
+      <CardHeader className="mx-auto">
+        <CardTitle className="text-center">Line Chart - Multiple</CardTitle>
+        <CardDescription className="text-center">January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer className="aspect-auto h-[250px] w-full" config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+            height={200}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <Line
+              dataKey="desktop"
+              type="monotone"
+              stroke="var(--color-desktop)"
+              strokeWidth={2}
+              dot={false}
+            />
+            <Line
+              dataKey="tab"
+              type="monotone"
+              stroke="var(--color-tab)"
+              strokeWidth={2}
+              dot={false}
+            />
+            <Line
+              dataKey="mobile"
+              type="monotone"
+              stroke="var(--color-mobile)"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
--- src/app/(tracker)/reporting/page.tsx
+++ src/app/(tracker)/reporting/page.tsx
@@ -1,14 +1,22 @@
+import { HomeChart } from "@/app-component/chart/HomeChart"
 import { CallChart } from "@/app-component/reporting/CallChart"
 
 export default function Page() {
     return (
-        <div className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <div className="p-4">
             <div className="flex items-center">
                 <h1 className="text-lg font-semibold md:text-2xl">Reporting</h1>
             </div>
-            <div
-                
+            <div   
             >
+                <div className="flex flex-row max-md:flex-col">
+                    <div className="w-9/12 max-md:w-full">
+                        <HomeChart/>
+                    </div>
+                    <div className="w-3/12 max-md:w-full">
+                        hello
+                    </div>
+                </div>
             </div>
         </div>
     )
