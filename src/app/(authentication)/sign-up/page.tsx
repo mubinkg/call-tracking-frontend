f8a40495d561a000bded8f68f6e06e9722c43cb3
mubin <mubin.ice.ru@gmail.com>
2024-08-26 18:53:22 +0600

    update signin and signup page

--- src/app/(authentication)/sign-up/page.tsx
+++ src/app/(authentication)/sign-up/page.tsx
@@ -1,59 +1,63 @@
+import Image from "next/image"
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 export default function Page() {
     return (
-        <div className="min-h-[75vh]">
-            <div className="mt-[25vh]">
-                <Card className="mx-auto max-w-sm">
-                    <CardHeader>
-                        <CardTitle className="text-xl text-center">Sign Up</CardTitle>
-                        <CardDescription>
+        <div className="w-full lg:grid lg:min-h-[100vh] lg:grid-cols-2 xl:min-h-[100vh]">
+            <div className="flex items-center justify-center py-12">
+                <div className="mx-auto grid w-[350px] gap-6">
+                    <div className="grid gap-2 text-center">
+                        <h1 className="text-3xl font-bold">Sign Up</h1>
+                        <p className="text-balance text-muted-foreground">
                             Enter your information to create an account
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <div className="grid gap-4">
-                            <div className="grid gap-2">
-                                <Label htmlFor="email">Email</Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="m@example.com"
-                                    required
-                                />
-                            </div>
-                            <div className="grid gap-2">
-                                <Label htmlFor="password">Password</Label>
-                                <Input id="password" type="password" />
-                            </div>
-                            <div className="grid gap-2">
-                                <Label htmlFor="password">Confirm Password</Label>
-                                <Input id="password" type="password" />
-                            </div>
+                        </p>
+                    </div>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label htmlFor="password">Password</Label>
+                            <Input id="password" type="password" />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label htmlFor="password">Confirm Password</Label>
+                            <Input id="password" type="password" />
+                        </div>
+                        <Link href='/reporting'>
                             <Button type="submit" className="w-full">
                                 Create an account
                             </Button>
-                        </div>
-                        <div className="mt-4 text-center text-sm">
-                            Already have an account?{" "}
-                            <Link href="/" className="underline">
-                                Sign in
-                            </Link>
-                        </div>
-                    </CardContent>
-                </Card>
+                        </Link>
+                    </div>
+                    <div className="mt-4 text-center text-sm">
+                        Already have an account?{" "}
+                        <Link href="/" className="underline">
+                            Sign in
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="hidden bg-muted lg:block">
+                <Image
+                    src="/calls.webp"
+                    alt="Image"
+                    width="1920"
+                    height="1080"
+                    className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                />
             </div>
         </div>
     )
 }
+
